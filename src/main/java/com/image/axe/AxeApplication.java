f0277087de17b4fb548f0ecf497d1b26f129e779
Yasamin Klingler <yasamin.klingler@getyourguide.com>
2023-02-10 22:54:20 +0100

    initial spring boot for ax web.

---
+++ src/main/java/com/image/axe/AxeApplication.java
@@ -0,0 +1,13 @@
+package com.image.axe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/image/axe/AxeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.image.axe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
